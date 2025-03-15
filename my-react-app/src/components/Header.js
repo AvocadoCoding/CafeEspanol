41bdbfeae0b8e32a0cfb6de53aa0f2b24ce6815f
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-03-15 18:36:14 +0000

    Header sizing image modification

--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import headerImage from '../assets/images/Header.JPG'; 
+import headerImage from '../assets/images/Header.JPG';
 import './Header.css';
 
 function Header() {
   return (
-    <header
-      className="site-header"
-      style={{ backgroundImage: `url(${headerImage})` }}
-    >
+    <header className="site-header">
+      <div className="header-content">
+        <img src={headerImage} alt="Header" className="header-image" />
+      </div>
     </header>
   );
 }
