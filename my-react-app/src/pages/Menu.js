0a45cbbd8d8443b6975cbf53139d825b5ccbc0e4
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-03-16 19:30:00 +0000

    Menu style changes

--- my-react-app/src/pages/Menu.js
+++ my-react-app/src/pages/Menu.js
@@ -4,8 +4,26 @@ import React from 'react';
 function Menu() {
   return (
     <div style={{ backgroundColor: '#FFF9DB', minHeight: '100vh' }}>
-      <div className="home-content" style={{ padding: '1rem', textAlign: 'center' }}>
-        <h2>Menú</h2>
+      <div style={{ height: '2rem', backgroundColor: '#FFF9DB' }}></div>
+      <div className="page-content" style={{ 
+        backgroundColor: '#f5c943', 
+        padding: '2rem', 
+        textAlign: 'center', 
+        maxWidth: '400px', 
+        margin: '0 auto',
+        border: '5px solid', // width of the border
+        borderImage: 'linear-gradient(to right, #333, #ccc) 1', // gradient border
+        borderRadius: '15px', // rounded corners
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' // subtle shadow for depth
+         }}>
+      <h2>Menú</h2>
+        <p></p>
+        <p>Caldo de gallina</p>
+        <p>Pollo broaster</p>
+        <p>Salchi broaster</p>
+        <p>Salchipapa</p>
+        <p>Bebidas calientes</p>
+        <p>Gaseosas</p>
         {/* Add more home page content here */}
       </div>
     </div>
